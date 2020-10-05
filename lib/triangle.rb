e29bd1452d8ebc92b8e30dfe2ee7c9ecaa51948e
Pbsmith82 <pbsmith82@gmail.com>
2020-10-05 11:46:51 -0400

    triangle-custom-error

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  # triangle code
+ 
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c 
+  end
+
+  def kind 
+    if @a<=0 or @b<=0 or @c<=0
+      raise TriangleError 
+    elsif @a+@b<=@c or @b+@c<=@a or @a+@c<=@b
+      raise TriangleError 
+    elsif @a==@b and @a==@c
+      return :equilateral                  
+    elsif @a==@b or @b==@c or @a==@c
+      return :isosceles 
+    else 
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
