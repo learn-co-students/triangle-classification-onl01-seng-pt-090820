518d00f69bf4ed67e5160eec640add7ade50a5bb
Chase090 <carloyam21@icloud.com>
2020-10-16 03:40:19 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,9 +5,9 @@ class Triangle
     @sides = []
     # set an array maybe?
 
-    def initialize (a, b, c)
-      @sides = [a, b, c]
-    @sides.sort!
+    def initialize (a, b, c) # triangle.new(3,2,2)
+      @sides = [a, b, c] # argument passed through are the sides abc
+    @sides.sort! # sort! array
     # knows that triangles violating triangle inequality are illegal 2 and 3
     end
 
@@ -18,8 +18,9 @@ class Triangle
   # of the third side. This is a principle known as the _triangle inequality_.
   def kind
     if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+    # if any of the sides is equal or less than 0 or if the sum of 2 sides is <= to side 3
       raise TriangleError
-    elsif @sides.uniq.length == 1
+    elsif @sides.uniq.length == 0 
       :equilateral
     elsif @sides.uniq.length == 2
       :isosceles
