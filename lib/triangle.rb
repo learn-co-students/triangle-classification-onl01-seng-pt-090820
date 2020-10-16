4fcdb1306ba6f1ed9c2995f466f6a77b599930ae
Chase090 <carloyam21@icloud.com>
2020-10-16 03:41:26 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
     if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
     # if any of the sides is equal or less than 0 or if the sum of 2 sides is <= to side 3
       raise TriangleError
-    elsif @sides.uniq.length == 0 
+    elsif @sides.uniq.length == 1 
       :equilateral
     elsif @sides.uniq.length == 2
       :isosceles
