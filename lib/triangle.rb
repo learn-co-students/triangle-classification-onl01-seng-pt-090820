0461be0e8008856eca6ab751fa35c7409276ee5c
Chase090 <carloyam21@icloud.com>
2020-10-16 03:31:46 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Triangle
-  # write code here
-end
+    attr_accessor :sides
+
+    @sides = []
+    # set an array maybe?
+
+    def initialize (a, b, c)
+      @sides = [a, b, c]
+    @sides.sort!
+    # knows that triangles violating triangle inequality are illegal 2 and 3
+    end
+
+
+
+# The sum of the lengths of any two sides of a triangle always exceeds the length
+#side 1 + side 2 > side 3
+  # of the third side. This is a principle known as the _triangle inequality_.
+  def kind
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+     
+      class TriangleError < StandardError
+      end
+    
+    
+    
+    end
+
